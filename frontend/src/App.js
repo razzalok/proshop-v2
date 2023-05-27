e816e8f7488551cd327841ab795597740de0490e
razzalok <alokraj7184@gmail.com>
2023-05-27 14:19:06 +0530

    added logo and displayed items on main screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
+import {Container} from 'react-bootstrap'
+import Header from "./components/Header";
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen'
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to ProShop</h1>
-    </div>
+    <>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to ProShop</h1>
+          <HomeScreen/>
+        </Container>
+      </main>
+      <Footer/>
+    </>
   );
 }
 
