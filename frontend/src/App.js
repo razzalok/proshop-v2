5f06a6c298a6c02b554fa7b2d2bb0b83d365383a
razzalok <alokraj7184@gmail.com>
2023-05-27 15:08:06 +0530

    Adding Review and Rating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import {Container} from 'react-bootstrap'
+import {Outlet} from 'react-router-dom'
 import Header from "./components/Header";
 import Footer from './components/Footer';
-import HomeScreen from './screens/HomeScreen'
 function App() {
   return (
     <>
       <Header/>
       <main className='py-3'>
         <Container>
-          <h1>Welcome to ProShop</h1>
-          <HomeScreen/>
+          <Outlet/>
+         
         </Container>
       </main>
       <Footer/>
