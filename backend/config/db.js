31ec9d6ddc400395aead4b3807db5c5d5678770b
razzalok <alokraj7184@gmail.com>
2023-05-31 14:10:13 +0530

    change in connection string

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,13 @@
-import mongoose from "mongoose"
-import dotenv from 'dotenv'
+import mongoose from 'mongoose';
 
-dotenv.config()
-
-const connectDB = async ()=>{
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${conn.Connection.host}`);
-    } catch (error) {
-        console.log(`Error: ${error.message}`);
-        process.exit(1)
-    }
-}
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
